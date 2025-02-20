429aec9f609e084ecb2fb79aeb75acaed0599add
Aravind Shanmugam <91383588+AravindShanmugam02@users.noreply.github.com>
2025-02-20 13:55:47 +0000

    Added a comment explaining Do(s) and Don't(s) of Forward Declaration

--- CopyCommand.h
+++ CopyCommand.h
@@ -3,6 +3,14 @@
 #include "CommandBase.h"
 
 class Receiver;
+// When forward declaring, we can only do certain things like:
+// Declare variables (or members) of type "pointer to A" and "reference to A"
+// Declare functions which take arguments of type A or return type A
+// You can't:
+// Declare variables(nor members) of type A
+// Dereference pointers to A or access any members of references to A
+// Define subclasses of A.
+
 class CopyCommand : public CommandBase
 {
 public:
